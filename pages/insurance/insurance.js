f21e0f9de5bd528afa7efee00cf8f83b112cad88
2015lym <devilx2015@gmail.com>
2019-10-16 15:19:41 +0800

    更新

--- pages/insurance/insurance.js
+++ pages/insurance/insurance.js
@@ -27,48 +27,4 @@ Page({
   onReady: function () {
 
   },
-
-  downloadImage: function ()  {
-    $Toast({
-      content: '下载图片需要在管理后台更新域名配置',
-      type: 'error'
-    });
-    // var imgSrc = "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=666915515,3161272054&fm=26&gp=0.jpg"
-    // wx.downloadFile({
-    //   url: imgSrc,
-    //   success: function (res) {
-    //     console.log(res);
-    //     //图片保存到本地
-    //     wx.saveImageToPhotosAlbum({
-    //       filePath: res.tempFilePath,
-    //       success: function (data) {
-    //         wx.showToast({
-    //           title: '保存成功',
-    //           icon: 'success',
-    //           duration: 2000
-    //         })
-    //       },
-    //       fail: function (err) {
-    //         console.log(err);
-    //         if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
-    //           console.log("当初用户拒绝，再次发起授权")
-    //           wx.openSetting({
-    //             success(settingdata) {
-    //               console.log(settingdata)
-    //               if (settingdata.authSetting['scope.writePhotosAlbum']) {
-    //                 console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
-    //               } else {
-    //                 console.log('获取权限失败，给出不给权限就无法正常使用的提示')
-    //               }
-    //             }
-    //           })
-    //         }
-    //       },
-    //       complete(res) {
-    //         console.log(res);
-    //       }
-    //     })
-    //   }
-    // })
-  }
 })
